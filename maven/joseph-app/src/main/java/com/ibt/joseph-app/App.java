ae004333f71680cafc38aef721fceb28b65ccb32
Oluwadare <adaramola01@yahoo.com>
2023-12-19 15:23:43 -0600

    updating version

---
+++ maven/joseph-app/src/main/java/com/ibt/joseph-app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.joseph_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World-this is joseph"); }
+    }
+
